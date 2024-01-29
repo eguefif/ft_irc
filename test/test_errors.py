7bc37af83e9facf91df69e6be3099b4e38b32d07
eguefif <eguefif@fastmail.com>
2024-01-29 17:55:24 -0500

    Fixed error test

--- test/test_errors.py
+++ test/test_errors.py
@@ -7,24 +7,32 @@ from settings import *
 
 test_command = [
         (b"NICK " + SEP, f"{PREFIX} 431 :No nickname given", "Invalid NICK command: 127.0.0.1"),
-        (b"NICK 1234567890" + SEP, f"{PREFIX} 432 * 1234567890 :Erroneous Nickname", "Invalid NICK command: 127.0.0.1"),
-        (b"NICK 123456789 0123" + SEP, f"{PREFIX} 432 * 1234567890 0123 :Erroneous Nickname", "Invalid NICK command: 127.0.0.1"),
+        (b"NICK 1234567890" + SEP, f"{PREFIX} 432 * 1234567890 :Erroneous nickname", "Invalid NICK command: 127.0.0.1"),
+        (b"NICK 123456789 0123" + SEP, f"{PREFIX} 432 * 123456789 0123 :Erroneous nickname", "Invalid NICK command: 127.0.0.1"),
         ]
 
 async def get_read_content(reader):
     try:
         async with asyncio.timeout(0.1):
-            retval = await reader.readline()
-            retval2 = await reader.readline()
+            _ = await reader.readline()
     except TimeoutError:
         return ""
+    try:
+        async with asyncio.timeout(0.1):
+            retval = await reader.readline()
+    except TimeoutError:
+        return ""
+    try:
+        async with asyncio.timeout(0.1):
+            retval2 = await reader.readline()
+    except TimeoutError:
+        return retval
     return [retval, retval2]
     
 @pytest.mark.asyncio
 @pytest.mark.parametrize("command,server_return,log", test_command)
 async def test_errors(clean_log, command, server_return, log):
     reader, writer = await asyncio.open_connection(HOST, PORT)
-    _ = await reader.readline()
     writer.write(command)
     retval = await get_read_content(reader)
     writer.close()
@@ -36,10 +44,9 @@ async def test_errors(clean_log, command, server_return, log):
         to_cmp = content[1].strip()
         to_cmp = to_cmp.split("] ")
         assert to_cmp[1] == log
-        print(retval)
-        if len(server_return):
-            assert retval[0] == server_return
-            assert len(retval[1]) == 0
+        print(retval.decode().strip())
+        if len(server_return) and type(retval) is not list:
+            assert retval.decode().strip() == server_return
         else:
-            assert len(retval) == 0
+            assert len(retval) != 2
 
